a0550c5dd946221fca76abf61942c6a7d782c3e2
bogdanmosica <bogdan.mosica@gmail.com>
2023-09-03 19:43:12 +0300

    fixed types in build for web

--- apps/web/app/(marketing)/page.tsx
+++ apps/web/app/(marketing)/page.tsx
@@ -1,8 +1,4 @@
 'use client';
-import Link from 'next/link';
-
-import { env } from '../../env.mjs';
-import { siteConfig } from '../../config/site';
 import { HomeHeroSection } from '../../components/home/home-hero-section';
 import { HomeFeaturesSection } from '../../components/home/home-features-section';
 import { HomeTestimonialsSection } from '../../components/home/home-testimonials-section';
