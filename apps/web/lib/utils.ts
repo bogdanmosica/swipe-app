42e713c223abce51db723c80d5ed41b3356875fd
bogdanmosica <bogdan.mosica@gmail.com>
2023-09-05 21:15:20 +0300

    update backend_url

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,10 +1,9 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export const SWIPE_API_URL =
-  process.env.NODE_ENV === 'production' && process.env.BACKEND_URL
-    ? process.env.BACKEND_URL
-    : 'http://localhost:3000/api';
+export const SWIPE_API_URL = process.env.BACKEND_URL
+  ? process.env.BACKEND_URL
+  : 'http://localhost:3000/api';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
