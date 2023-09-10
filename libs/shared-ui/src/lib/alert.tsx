249191c9ad4863417bcf4859896fb36ff343b174
bogdanmosica <bogdan.mosica@gmail.com>
2023-09-10 20:16:24 +0300

    did some fix in shadcn components

--- libs/shared-ui/src/lib/alert.tsx
+++ libs/shared-ui/src/lib/alert.tsx
@@ -35,24 +35,28 @@ Alert.displayName = 'Alert';
 const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <h5
     ref={ref}
     className={cn('mb-1 font-medium leading-none tracking-tight', className)}
     {...props}
-  />
+  >
+    {children}
+  </h5>
 ));
 AlertTitle.displayName = 'AlertTitle';
 
 const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <div
+>(({ className, children, ...props }, ref) => (
+  <p
     ref={ref}
     className={cn('text-sm [&_p]:leading-relaxed', className)}
     {...props}
-  />
+  >
+    {children}
+  </p>
 ));
 AlertDescription.displayName = 'AlertDescription';
 
